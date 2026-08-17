af4cfeab90d0bfe9c3beebfc2d2b3f7d3c181d44
Radhika <radhikabansal365@gmail.com>
2026-08-17 11:57:13 +0530

    feat: initialize TodoContextLocal project with Vite, React, and Tailwind CSS

---
+++ 09_TodoContextLocal/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-pink-800 text-white p-6 text-center text-2xl font-bold">
+      App
+    </div>
+  );
+};
+
+export default App;
